f344ff24f44e5b253275bdfa73ed0ae0dddd62e4
JayDub21 <justin.wofford.21@gmail.com>
2020-12-21 15:55:42 -0600

    date and time formatting

---
+++ usefulFunctions/concatFirstLastName.sql
@@ -0,0 +1,5 @@
+USE sql_store;
+
+SELECT CONCAT(first_name, ' ', last_name) 
+	AS full_name
+FROM customers
---
+++ usefulFunctions/dateTimeFormatting.sql
@@ -0,0 +1,6 @@
+-- USE sql_store;
+
+-- SELECT EXTRACT(DA FROM NOW())
+
+-- SELECT DATE_FORMAT(NOW(), '%M %d %Y');
+SELECT TIME_FORMAT(NOW(), '%h: %i %p')
