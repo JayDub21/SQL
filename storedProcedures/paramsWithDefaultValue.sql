c30ad3d40d56d67a8770521364ca5caedead0db7
JayDub21 <justin.wofford.21@gmail.com>
2020-12-21 23:01:51 -0600

    parameter validation

--- storedProcedures/paramsWithDefaultValue.sql
+++ storedProcedures/paramsWithDefaultValue.sql
@@ -1,16 +1,42 @@
--- Returns all state if param is NULL
+-- Write a stored procedure called get_payments
+-- with two paraments:
+-- client_id => INT
+-- payment_method_id => TINYINT
 
-DROP PROCEDURE IF EXISTS get_clients_by_state;
+DROP PROCEDURE IF EXISTS get_payments;
 
 DELIMITER $$
-CREATE PROCEDURE get_clients_by_state
+CREATE PROCEDURE get_payments
 (
-	state CHAR(2)
+	client_id INT,
+    payment_method_id TINYINT
 )
 BEGIN
-		SELECT * FROM clients c
-		WHERE c.state = IFNULL(state, c.state);
+		SELECT * 
+        FROM payments p
+        WHERE
+			p.client_id = IFNULL(client_id, p.client_id) AND
+            p.payment_method = IFNULL(payment_method_id, p.payment_method);
 
 END $$
 
 DELIMITER ;
+
+
+
+-- Returns all state if param is NULL
+
+-- DROP PROCEDURE IF EXISTS get_clients_by_state;
+
+-- DELIMITER $$
+-- CREATE PROCEDURE get_clients_by_state
+-- (
+-- 	state CHAR(2)
+-- )
+-- BEGIN
+-- 		SELECT * FROM clients c
+-- 		WHERE c.state = IFNULL(state, c.state);
+
+-- END $$
+
+-- DELIMITER ;
