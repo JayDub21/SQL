b7787c0693cf6b80d705473c0949b5c22ef37c6a
JayDub21 <justin.wofford.21@gmail.com>
2021-01-19 17:11:25 -0600

    Update createTransaction.sql

--- transactions/createTransaction.sql
+++ transactions/createTransaction.sql
@@ -1,13 +1,6 @@
--- creating a transaction
-
--- USE sql_store;
-
+USE sql_store;
 START TRANSACTION;
-
-INSERT INTO orders (customer_id, order_date, status)
-VALUES (1, '2020-01-01', 1);
-
-INSERT INTO order_items
-VALUES (LAST_INSERT_ID(), 1, 1, 1);
-
+UPDATE customers
+SET points = points + 10
+WHERE customer_id = 1;
 COMMIT;
